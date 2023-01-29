45178a2f4dca949268d3c185d1b9fbf0ddb443d0
AncaGhenade <anca.ghenade@ilogics.com>
2023-01-29 22:52:29 +0100

    update create-lambda script

--- create-lambda.sh
+++ create-lambda.sh
@@ -1,38 +1,33 @@
 #!/bin/bash
 
-# Set the name of the Lambda function
-function_name="shipment-list-lambda-validator"
-aws_region="eu-central-1"
-
-# Set the name of the S3 bucket where the JAR file will be uploaded
-app_source_bucket_name="shipment-list-lambda-validator-bucket"
-file_source_bucket_name="shipment-list-demo-bucket"
-
 # Build the application
-mvn clean install
+mvn clean package shade:shade
 
-# Create the S3 bucket
-aws s3api create-bucket --bucket $app_source_bucket_name --create-bucket-configuration LocationConstraint=$aws_region
+# Create the S3 bucket where the JAR file will be uploaded
+aws s3api create-bucket --bucket shipment-list-lambda-validator-bucket --region eu-central-1 \
+--create-bucket-configuration LocationConstraint=eu-central-1
+
 
 # Upload the JAR file to S3
-aws s3 cp target/shipment-list-lambda-validator-1.0-SNAPSHOT.jar s3://$app_source_bucket_name/
+aws s3 cp target/shipment-list-lambda-validator-1.0-SNAPSHOT.jar s3://shipment-list-lambda-validator-bucket/
 
 # Create the Lambda function
 aws lambda create-function \
     --function-name shipment-list-lambda-validator \
     --runtime java11 \
-    --handler dev.ancaghenade.shipmentlistlambdavalidator.ServiceHandler::handleRequest \
-    --code S3Bucket=$app_source_bucket_name,S3Key=shipment-list-lambda-validator-1.0-SNAPSHOT.jar \
     --region eu-central-1 \
+    --handler dev.ancaghenade.shipmentlistlambdavalidator.ServiceHandler::handleRequest \
+    --code S3Bucket=shipment-list-lambda-validator-bucket,S3Key=shipment-list-lambda-validator-1.0-SNAPSHOT.jar \
     --memory 512 \
     --timeout 15 \
-    --role arn:aws:iam::932043840972:role/shipment-list-demo-role
+    --role arn:aws:iam::{YOUR_AWS_ACCOUNT_ID}:role/shipment-list-demo-role
+
 
 # Grant S3 Invoke Lambda Permission
-aws lambda add-permission --function-name $function_name \
+aws lambda add-permission --function-name shipment-list-lambda-validator \
      --statement-id s3-invoke --action "lambda:InvokeFunction" \
      --principal s3.amazonaws.com \
-     --source-arn arn:aws:s3:::$file_source_bucket_name
+     --source-arn arn:aws:s3:::shipment-list-demo-bucket
 
 # If you need to re-deploy the jar directly:
 
